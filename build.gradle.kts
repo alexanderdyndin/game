02639a54be881b1f4be8d658a0125da2893fc207
ALEXANDER DYNDIN <Like81@yandex.ru>
2024-01-06 17:01:14 +0300

    init game

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+
+plugins {
+	alias(libs.plugins.korge) version "4.0.0-rc"
+}
+
+korge {
+	id = "com.example.example"
+	targetJvm()
+    targetJs() // comment this to not build for javascript
+    targetDesktop()
+    targetIos()
+    targetAndroidIndirect()
+}
